cb4fc7daf41951058418ee94e420138118816643
Marco La Rosa <m@lr.id.au>
2014-09-11 19:04:11 +1000

    add in the url path to the images

--- indexer/EADProcessor.py
+++ indexer/EADProcessor.py
@@ -90,7 +90,7 @@ class EADProcessor:
                 if self.images is not None:
                     # stash the image path
                     image_path = etree.Element('field', name='image_path')
-                    image_path.text = self.images
+                    image_path.text = metadata['image_base']
                     d.append(image_path)
 
                     # generate the list of small images
